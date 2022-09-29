33bfc2d089e692560632e84863bea11727ae17eb
Pavel Březina <pbrezina@redhat.com>
2022-09-29 12:30:42 +0200

    ad: add dc.ad.test to setup-dns-files.sh

--- src/tools/setup-dns-files.sh
+++ src/tools/setup-dns-files.sh
@@ -14,9 +14,11 @@ sed -i '/master.ipa.test/d' /etc/hosts
 sed -i '/master.ldap.test/d' /etc/hosts
 sed -i '/dc.samba.test/d' /etc/hosts
 sed -i '/client.test/d' /etc/hosts
+sed -i '/dc.ad.test/d' /etc/hosts
 
 # Append the lines
 echo "172.16.100.10 master.ipa.test" >> /etc/hosts
 echo "172.16.100.20 master.ldap.test" >> /etc/hosts
 echo "172.16.100.30 dc.samba.test" >> /etc/hosts
 echo "172.16.100.40 client.test" >> /etc/hosts
+echo "172.16.200.10 dc.ad.test" >> /etc/hosts
