56de71b5a51635ef7056ad522d681f1b277ab388
Dan Lavu <dlavu@redhat.com>
2025-05-02 14:54:49 -0400

    moving ipa2 to a second subnet
    * making this environment more realistic
    * moved the network create to use the docker command because
      docker-compose defaults the isolation value to true

---
+++ src/tools/create-networks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Create sssd-ci networks.
+#
+# Networks created using docker-compose sets 'isolation = true', making hosts
+# in other networks unroutable. A workaround is to create the networks using
+# the docker command and define the network as external in docker-compose.
+#
+# Usage:
+#   create-networks.sh
+#
+
+set -xe
+source ./src/tools/get-container-engine.sh
+
+${DOCKER} network inspect sssd-ci || \
+    ${DOCKER} network create \
+    --driver bridge \
+    --subnet 172.16.100.0/24 \
+    --gateway 172.16.100.1 \
+    sssd-ci
+${DOCKER} network inspect ipa-ci || \
+    ${DOCKER} network create \
+    --driver bridge \
+    --subnet 172.16.110.0/24 \
+    --gateway 172.16.110.1 \
+    ipa-ci
